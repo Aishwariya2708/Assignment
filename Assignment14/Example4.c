1f3ac8b83e1a42c10325d91f30d0018ba35a810f
Aishwariya2708 <bokefodeaishwariya@gmail.com>
2025-05-28 15:31:06 +0530

    Add files via upload

---
+++ Assignment14/Example4.c
@@ -0,0 +1,44 @@
+// 4. Accept number of rows  and  number of column from usser and display below pattern 
+
+#include<stdio.h>
+ 
+void Pattern(int iRow,int iCol)
+{
+int i=0 ,j=0;
+int iNo=0;
+
+        for(i=1;i<=iRow;i++)
+        {
+            for(j=1;j<=iCol;j++)
+            {
+                if(i%2==0)
+                {
+                  iNo=-iNo;
+                    printf("-%d",j);
+                }
+                else
+                {
+                      printf("%d",j);
+                }
+            }
+    printf("\n");
+
+}
+}
+
+   
+
+
+int main ()
+
+{
+    
+    int iValue1=0,iValue2=0;
+
+    printf("Enter number of rows and columns:");
+    scanf("%d %d",&iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
