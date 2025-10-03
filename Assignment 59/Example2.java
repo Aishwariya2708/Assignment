964fbdf40bd3ea1105fba0abda681c325ee78b98
Aishwariya2708 <bokefodeaishwariya@gmail.com>
2025-10-03 13:16:19 +0530

    Add files via upload

---
+++ Assignment 59/Example2.java
@@ -0,0 +1,60 @@
+/* 2. Write java program to print below pattern. 
+       *
+      *  *
+      *  *  *
+      *  *  *  *
+      *  *  *  *  *
+*/
+
+
+
+
+import java.util.*;
+
+class PrintPattern
+{
+  public void Display(int iRow,int iCol)
+  {
+    int i=0,j=0;
+
+    if(iRow !=iCol)
+    {
+      System.out.println("Invalid input");
+
+      return;
+    }
+        
+    for(i=1;i<=iRow;i++)
+    {
+      for(j=1;j<=i;j++)
+      {
+        System.out.print("*\t");
+      }
+      System.out.println();
+    }
+  }
+}
+        
+
+class Example2
+{
+    public static void main(String A[])
+    {
+      Scanner sobj = new Scanner(System.in);
+
+       int iValue1=0, iValue2=0 ;
+
+          System.out.println("Enter number of Rows:");
+          iValue1=sobj.nextInt();
+         
+          
+          System.out.println("Enter number of Columns:");
+          iValue2=sobj.nextInt();
+
+         PrintPattern  pobj =  new PrintPattern();
+
+          pobj.Display(iValue1, iValue2);
+    }
+  }
+
+
---
+++ Assignment 59/Example3.java
@@ -0,0 +1,60 @@
+/* 3. Write java program to print below pattern. 
+   
+* * * * *
+* * * *
+* * *
+* *
+*
+
+*/
+
+
+import java.util.*;
+
+class PrintPattern
+{
+  public void Display(int iRow,int iCol)
+  {
+    int i=0,j=0;
+
+    if(iRow !=iCol)
+    {
+      System.out.println("Invalid input");
+
+      return;
+    }
+        
+    for(i=iRow;i>=1;i--)
+    {
+      for(j=1;j<=i;j++)
+      {
+        System.out.print("*\t");
+      }
+      System.out.println();
+    }
+  }
+}
+        
+
+class Example3
+{
+    public static void main(String A[])
+    {
+      Scanner sobj = new Scanner(System.in);
+
+       int iValue1=0, iValue2=0 ;
+
+          System.out.println("Enter number of Rows:");
+          iValue1=sobj.nextInt();
+         
+          
+          System.out.println("Enter number of Columns:");
+          iValue2=sobj.nextInt();
+
+         PrintPattern  pobj =  new PrintPattern();
+
+          pobj.Display(iValue1, iValue2);
+    }
+  }
+
+
